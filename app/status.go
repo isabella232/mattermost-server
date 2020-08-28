74c28d5fef28842c158e092be5e2889c484012a1
Prathik Kaliyambath <prathik94@gmail.com>
2020-08-29 02:32:12 +0530

    removed fmt

--- app/status.go
+++ app/status.go
@@ -6,7 +6,6 @@ package app
 import (
 	"github.com/mattermost/mattermost-server/v5/mlog"
 	"github.com/mattermost/mattermost-server/v5/model"
-	"fmt"
 )
 
 func (a *App) AddStatusCacheSkipClusterSend(status *model.Status) {
